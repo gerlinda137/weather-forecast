a4d15ce90c44e9b3fb3aa13202e2416b888a9efe
gerlinda137 <gerlinda137@gmail.com>
2021-02-11 22:54:11 +0300

    rebuild current according to new data

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ for (let i = 0; i < forecastDays.length; i++) {
 // ).textContent = currentDate.toLocaleDateString('en-US', dateOptions);
 
 fetch(
-  'https://api.openweathermap.org/data/2.5/weather?id=498817&appid=3e352cf401fb565c887aab84536ac798'
+  'https://api.openweathermap.org/data/2.5/onecall?lat=59.93863&lon=30.31413&exclude=hourly,minutely&appid=3e352cf401fb565c887aab84536ac798'
 )
   .then(function (resp) {
     return resp.json();
@@ -47,72 +47,74 @@ fetch(
   .then(function (data) {
     console.log(data);
     document.querySelector('.current__temperature-range').innerHTML =
-      Math.round(data.main.temp_min - 273) +
+      Math.round(data.daily[0].temp.min - 273) +
       '&deg;' +
       ' C ' +
       '/ ' +
-      Math.round(data.main.temp_max - 273) +
+      Math.round(data.daily[0].temp.max - 273) +
       '&deg;' +
       ' C';
     document.querySelector('.current__temperature').innerHTML =
-      Math.round(data.main.temp - 273) + '&deg;' + ' C';
+      Math.round(data.current.temp - 273) + '&deg;' + ' C';
     document.querySelector('.current__weather').textContent =
-      data.weather[0]['description'];
+      data.current.weather[0]['description'];
     document.querySelector('.current__feeling').innerHTML =
       'Feels like: ' +
       ' ' +
-      Math.round(data.main['feels_like'] - 273) +
+      Math.round(data.current['feels_like'] - 273) +
       '&deg;' +
       ' C';
     document.querySelector(
       '.current__img'
-    ).src = `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`;
+    ).src = `https://openweathermap.org/img/wn/${data.current.weather[0]['icon']}@2x.png`;
     document.querySelector('.humidity').textContent =
-      data.main['humidity'] + '%';
+      data.current['humidity'] + '%';
+    console.log(data.current['humidity'] + '%');
     document.querySelector('.pressure').textContent =
-      data.main['pressure'] + ' pmh';
-    document.querySelector('.wind').textContent = data.wind['speed'] + ' m/s';
-    if (data.weather[0].main in weatherBackgrounds) {
-      document.querySelector('.inner-container').style.backgroundImage =
-        "url('" + weatherBackgrounds[data.weather[0].main] + "')";
-    }
+      data.current['pressure'] + ' pmh';
+    document.querySelector('.wind').textContent =
+      data.current['wind_speed'] + ' m/s';
+    // if (data.weather[0].main in weatherBackgrounds) {
+    //   document.querySelector('.inner-container').style.backgroundImage =
+    //     "url('" + weatherBackgrounds[data.weather[0].main] + "')";
+    // }
   })
   .catch(function () {
     // catch any errors
   });
 
-fetch(
-  'https://api.openweathermap.org/data/2.5/onecall?lat=59.93863&lon=30.31413&exclude=hourly,minutely&appid=3e352cf401fb565c887aab84536ac798'
-)
-  .then(function (resp) {
-    return resp.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    // document.querySelector('.main-header__city').textContent = data.name;
-    document.querySelector('.top-section__temperature-range').innerHTML =
-      Math.round(data.main.temp_min - 273) +
-      '&deg;' +
-      ' C ' +
-      '/ ' +
-      Math.round(data.main.temp_max - 273) +
-      '&deg;' +
-      ' C';
-    document.querySelector('.current__temperature').innerHTML =
-      Math.round(data.daily[0].temp - 273) + '&deg;' + ' C';
-    document.querySelector('.top-section__weather').textContent =
-      data.weather[0]['description'];
-    document.querySelector(
-      '.weather-image__img'
-    ).src = `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@4x.png`;
-    document.querySelector('.humidity').textContent =
-      data.main['humidity'] + '%';
-    document.querySelector('.wind').textContent = data.wind['speed'] + ' m/s';
-    if (data.weather[0].main in weatherBackgrounds) {
-      document.querySelector('.inner-container').style.backgroundImage =
-        "url('" + weatherBackgrounds[data.weather[0].main] + "')";
-    }
-  })
-  .catch(function () {
-    // catch any errors
-  });
+// fetch(
+//   'https://api.openweathermap.org/data/2.5/onecall?lat=59.93863&lon=30.31413&exclude=hourly,minutely&appid=3e352cf401fb565c887aab84536ac798'
+// )
+//   .then(function (resp) {
+//     return resp.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     // document.querySelector('.main-header__city').textContent = data.name;
+//     document.querySelector('.top-section__temperature-range').innerHTML =
+//       Math.round(data.main.temp_min - 273) +
+//       '&deg;' +
+//       ' C ' +
+//       '/ ' +
+//       Math.round(data.main.temp_max - 273) +
+//       '&deg;' +
+//       ' C';
+//     document.querySelector('.current__temperature').innerHTML =
+//       Math.round(data.daily[0].temp - 273) + '&deg;' + ' C';
+//     document.querySelector('.top-section__weather').textContent =
+//       data.weather[0]['description'];
+//     document.querySelector(
+//       '.weather-image__img'
+//     ).src = `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@4x.png`;
+//     document.querySelector('.humidity').textContent =
+//       data.main['humidity'] + '%';
+//     document.querySelector('.wind').textContent = data.wind['speed'] + ' m/s';
+//     if (data.weather[0].main in weatherBackgrounds) {
+//       document.querySelector('.inner-container').style.backgroundImage =
+//         "url('" + weatherBackgrounds[data.weather[0].main] + "')";
+//     }
+//   })
+//   .catch(function () {
+//     // catch any errors
+//   });
